f999a66a01fe9623fbb56c9703ae74a456bb9f55
Liangjp <jungg1996@gmail.com>
2019-04-07 17:15:11 +0800

    Optimize the way to input strings for comparison.

--- main.c
+++ main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include "edit_distance.h"
 
-int main() {
-    const char *X = "jary";
-    const char *Y = "jerry";
+int main(int argc, char *args[]) {
+    if (argc != 3) {
+        printf("./exec [string1] [string2]");
+        return 1;
+    }
+    const char *X = args[1];
+    const char *Y = args[2];
     int dist = 0;
     dist = cal_edit_distance(X, Y);
     printf("Edit distance: %d", dist);
